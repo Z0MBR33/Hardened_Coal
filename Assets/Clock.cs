b9847cb9124b554cc012590be5838f7e687a92ad
Z0MBR33 <pascal.plewa97@hotmail.de>
2018-04-26 23:38:17 +0200

    Clock and more

---
+++ Assets/Clock.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+    public float Minit;
+    public float Hours;
+    public bool Am;
+    public float Day;
+    public float TimeSpeed;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void FixedUpdate()
+    {
+        if(Minit < 60)
+        {
+            Minit = 0;
+            Hours = 1;
+            if( Hours = 12)
+            {
+
+            }
+        }
+    }
+}
